6875f62674102c16f2aa4a7791655fe60bde1c49
Lukas Galke <git@lpag.de>
2018-01-16 17:02:27 +0100

    prep instead of tmp subdir

--- analyze.sh
+++ analyze.sh
@@ -2,8 +2,8 @@ if [[ $# != 1 ]]; then
     echo "Please provide log file as argument."
     exit -1
 fi
-mkdir -p tmp/
+mkdir -p prep/
 fname=$(basename $1)
-cp $1 tmp/$fname.prep
-vim tmp/$fname.prep -c 'source prep.vim | write | quit'
-python3 analyze.py < tmp/$fname.prep
+cp $1 prep/$fname.prep
+vim prep/$fname.prep -c 'source prep.vim | write | quit'
+python3 analyze.py < prep/$fname.prep
--- retrieve.sh
+++ retrieve.sh
@@ -1,2 +1,3 @@
 # depends on locdb ssh entry
+mkdir -p data
 scp locdb:/home/anlausch/.pm2/logs/loc-db-production-out-1.log data/
